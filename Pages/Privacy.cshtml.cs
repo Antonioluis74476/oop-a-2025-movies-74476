81987f4ca94d5b0fb855fcab74ecd296872b780f
Antonioluis92 <74476@student.dorset-college.ie>
2025-03-27 12:06:43 +0000

    Initial commit for movies app

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace oop_a_2025_movies_74476.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
